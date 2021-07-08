c0ecf6c642a8d6b975d17bc72bc044c464edf1b7
Cas Albert Lubbers <c.a.lubbers@avisi.nl>
2021-07-08 19:56:08 +0200

    [AME-817] Add unittests and use -a flag for rsync

--- pkg/k8s/volume_test.go
+++ pkg/k8s/volume_test.go
@@ -34,3 +34,35 @@ func TestCreatePersistentVolumeClaim(t *testing.T) {
     assert.Equal(t, []v1.PersistentVolumeAccessMode{v1.ReadWriteOnce}, testPVC.Spec.AccessModes)
     assert.Equal(t, resource.MustParse("1"), *testPVC.Spec.Resources.Requests.Storage())
 }
+
+
+func TestNewVolumeMount(t *testing.T) {
+
+    volumeMount := &v1.VolumeMount{
+        Name:      "name",
+        MountPath: "path",
+        ReadOnly:  false,
+    }
+
+    assert.Equal(t, volumeMount, NewVolumeMount("name", "path", false))
+    assert.NotEqual(t, volumeMount, NewVolumeMount("name", "path", true))
+    assert.NotEqual(t, volumeMount, NewVolumeMount("name", "nopath", false))
+    assert.NotEqual(t, volumeMount, NewVolumeMount("noname", "path", false))
+}
+
+func TestNewPersistentVolumeClaimVolume(t *testing.T) {
+    volume := &v1.Volume{
+        Name: "name",
+        VolumeSource: v1.VolumeSource{
+            PersistentVolumeClaim: &v1.PersistentVolumeClaimVolumeSource{
+                ClaimName: "claimName",
+                ReadOnly:  false,
+            },
+        },
+    }
+
+    assert.Equal(t, volume, NewPersistentVolumeClaimVolume("name", "claimName", false))
+    assert.NotEqual(t, volume, NewPersistentVolumeClaimVolume("name", "claimName", true))
+    assert.NotEqual(t, volume, NewPersistentVolumeClaimVolume("name", "noclaimName", false))
+    assert.NotEqual(t, volume, NewPersistentVolumeClaimVolume("noname", "claimName", false))
+}
