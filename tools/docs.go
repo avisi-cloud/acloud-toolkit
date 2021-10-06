2b424f00236773228b4312ce3c58e63f0b06c99a
Thomas Kooi <t.j.kooi@avisi.nl>
2021-10-06 10:22:05 +0200

    AME-1035 improve generated documentation

--- tools/docs.go
+++ tools/docs.go
@@ -1,15 +1,55 @@
 package main
 
 import (
+	"fmt"
 	"log"
+	"path"
+	"path/filepath"
+	"strings"
+	"time"
 
 	"github.com/spf13/cobra/doc"
 	"gitlab.avisi.cloud/ame/acloud-toolkit/cmd/acloud-toolkit/app"
 )
 
+const fmTemplate = `---
+date: %s
+title: "%s"
+displayName: "%s"
+slug: %s
+url: %s
+description: ""
+lead: ""
+draft: false
+images: []
+menu:
+  docs:
+    parent: "%s-ref"
+weight: %d
+toc: true
+---
+`
+
 func main() {
 	cmd := app.NewACloudToolKitCmd(nil, nil, nil)
-	err := doc.GenMarkdownTree(cmd, "./docs")
+
+	weight := 760
+	filePrepender := func(filename string) string {
+		now := time.Now().Format(time.RFC3339)
+		name := filepath.Base(filename)
+		base := strings.TrimSuffix(name, path.Ext(name))
+		displayName := strings.TrimPrefix(base, cmd.Name()+"_")
+		url := "/references/" + cmd.Name() + "/" + strings.ToLower(base) + "/"
+		weight--
+		return fmt.Sprintf(fmTemplate, now, strings.Replace(base, "_", " ", -1), strings.Replace(displayName, "_", " ", -1), base, url, cmd.Name(), weight)
+	}
+	linkHandler := func(name string) string {
+		base := strings.TrimSuffix(name, path.Ext(name))
+		return "/references/" + cmd.Name() + "/" + strings.ToLower(base) + "/"
+	}
+
+	cmd.DisableAutoGenTag = true
+	err := doc.GenMarkdownTreeCustom(cmd, "./docs", filePrepender, linkHandler)
 	if err != nil {
 		log.Fatal(err)
 	}
